0f59d97d553f79c023e7b962a9f29d6a236a26cd
epunter <ethanrpunter@gmail.com>
2024-06-02 09:41:23 +0100

    fix: spacing for decoratedstats

--- app/src/main/java/com/ethanpunter/charactersheets/stats/DecoratedStat.kt
+++ app/src/main/java/com/ethanpunter/charactersheets/stats/DecoratedStat.kt
@@ -3,11 +3,14 @@ package com.ethanpunter.charactersheets.stats
 import android.graphics.Point
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.LinearLayout
 import androidx.appcompat.content.res.AppCompatResources
 import com.ethanpunter.charactersheets.databinding.DecoratedStatBinding
 
 class DecoratedStat(position: Point, name: String, value: String, private val decoration: Int) :
     BasicStat(position, name, value, null) {
+
+    override val customHeight: Int = LinearLayout.LayoutParams.WRAP_CONTENT
 
     override fun getView(inflater: LayoutInflater): View {
         val binding = DecoratedStatBinding.inflate(inflater)
